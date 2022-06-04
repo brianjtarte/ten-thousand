f37945e63804c951e00b45740ec910e67be7c9d7
Brendon <Brendonhampton1988@gmail.com>
2022-06-04 15:09:17 -0700

    game-logic roll dice all tests completed, refactored roll dice static method

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -10,9 +10,13 @@ class GameLogic:
 
     @staticmethod
     def roll_dice(num_dice):
-        die_value = random.randint(1, 6)
-        print(die_value)
+        list = []
+        for num in range(1,num_dice +1):
+            die_value = random.randint(1, 6)
+            list.append(die_value)
+        return list
 
 
 if __name__ == '__main__':
-    GameLogic.roll_dice(1)
+    # GameLogic.roll_dice(2)
+    print(GameLogic.roll_dice(2))
