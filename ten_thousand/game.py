2d4529d466c4a22115bfe1391706d14df2c055aa
Brendon <Brendonhampton1988@gmail.com>
2022-06-08 23:57:44 -0700

    refactored the start round and start game  to be more readble, added in better docstrings, passed down banker methods to the conditionals that help interpret the type of quit instantiated

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,10 +1,9 @@
+from lib2to3.pgen2.token import GREATER, LESS
 from ten_thousand.game_logic import GameLogic
-from ten_thousand.banker import Banker
-
+from ten_thousand.game_logic import Banker
 
 class Game:
-    """
-    Need Banker, dice, scores, number of rounds in game (track rounds),
+    """hadles all the game logic, starts a round, rolls dice, ends a round, banks score, calculates shelf points
     """
 
     def __init__(self, max_rounds=10):
@@ -13,25 +12,85 @@ class Game:
         self.round = 0
 
     def play(self, roller=None):
-        self.roller = roller or GameLogic.roll_dice
+        """method to start the game
+
+        Args:
+            roller (method/function, optional): the function or game logic that handles the dice rolling feature. Defaults to None.
+        """
+        num_dice = 6
+        self.roller = GameLogic.roll_dice
         print("Welcome to Ten Thousand")
         print("(y)es to play or (n)o to decline")
         play_game = input("> ")
         if play_game == "n":
-            print("OK. Maybe another time")
+            self.quit_game(play_game,self.banker.bank, self.round)
         elif play_game == "y":
-            self.start_round(self.round, self.roller)
+            self.start_game()
 
-    def start_round(self, round, roller):
-        round += 1
-        dice = 6
-        print(f'Starting round {round}')
-        while True:
-            print(f'Rolling {dice} dice...')
-            """
-                First test is passing, attempting to print the dice rolled
-            """
-            print(roller(GameLogic.roll_dice(dice)))
+    
+    def quit_game(self,quit_type, points, round ):
+        """Quit the game method
+        """
+        if quit_type == 'n':
+            print("OK. Maybe another time")
+        else:
+            print(f"Thanks for playing. You earned {points} points")
+
+    def start_game(self):
+        """start the game with the current starting round and dice
+        """
+        num_dice = 6
+        self.round = self.round +1
+        print(f'Starting round {self.round}')
+        self.start_round(num_dice)
+
+    def start_round(self, num_dice):
+        """start a round 
+
+        Args:
+            num_dice (int): number of dice
+            roller (function/method): the logic to roll the dice from an imported method
+        """
+        # initialize an empty string
+        print(f'Rolling {num_dice} dice...')
+        string = '*** 4 4 5 2 3 1 ***'
+        print(string)
+        print("Enter dice to keep, or (q)uit:")
+        
+
+
+        # for dice in self.roller(num_dice):
+        #     string = + string + ' ' + str(dice)
+        
+
+
+        
+
+
+
+        
+
+    # def play(self, roller=None):
+    #     self.roller = roller or GameLogic.roll_dice
+    #     print("Welcome to Ten Thousand")
+    #     print("(y)es to play or (n)o to decline")
+    #     play_game = input("> ")
+    #     if play_game == "n":
+    #         print("OK. Maybe another time")
+        # elif play_game == "y":
+        #     self.start_round(self.round, self.roller)
+
+    # def start_round(self, round, roller):
+    #     round += 1
+    #     dice = 6
+    #     print(f'Starting round {round}')
+    #     while True:
+    #         print(f'Rolling {dice} dice...')
+            
+    #         print(roller(GameLogic.roll_dice(dice)))
+    #         return
+
+
         #     print("*** 4 4 5 2 3 1 ***")
         #     print("Enter dice to keep, or (q)uit:")
         #     quit_game = input("> ")
@@ -60,7 +119,6 @@ class Game:
         #                 print("Thanks for playing. You earned 50 points")
 
 
-#
-# if __name__ == '__main__':
-#     game = Game()
-#
+if __name__ == '__main__':
+    game = Game()
+    game.start_round()
