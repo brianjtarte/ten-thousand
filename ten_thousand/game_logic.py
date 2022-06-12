95643c511931a5476ac348d89094ad83bf44d0c8
Marni Hager <mlh6118@gmail.com>
2022-06-11 23:13:17 -0700

    Fixed file formatting and deleted commented lines.

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -4,16 +4,17 @@ from collections import Counter
 
 # Define GameLogic class with roll_dice static method.
 class GameLogic:
-    ''' Game Logic: Contains the main methods for the game of calculating the score, rolling the dice, and banking. '''
+    """
+    Game Logic: Contains the main methods for the game of calculating the
+     score, rolling the dice, and banking.
+    """
 
     def __init__(self):
         pass
 
     @staticmethod
     def roll_dice(num_dice=6):
-
         roll = tuple(random.randint(1, 6) for die in range(0, num_dice))
-
         return roll
 
     @staticmethod
@@ -21,14 +22,12 @@ class GameLogic:
         score = 0
         count_pair = 0
         count = Counter(roll).most_common()
-
         num = len(count)
-
         for num in range(0, len(count)):
+
             #  ones
             if count[num][0] == 1 and count[num][1] <= 2:
                 score += 100 * count[num][1]
-                
 
             if count[num][0] == 1 and count[num][1] > 2:
                 score += 1000 * (count[num][1] - 2)
@@ -36,18 +35,22 @@ class GameLogic:
             # twos
             if count[num][0] == 2 and count[num][1] >= 3:
                 score += 200 * (count[num][1] - 2)
+
             # threes
             if count[num][0] == 3 and count[num][1] >= 3:
                 score += 300 * (count[num][1] - 2)
+
             # fours
             if count[num][0] == 4 and count[num][1] >= 3:
                 score += 400 * (count[num][1] - 2)
+
             # fives
             if count[num][0] == 5 and count[num][1] <= 2:
                 score += 50 * count[num][1]
 
             if count[num][0] == 5 and count[num][1] > 2:
                 score += 500 * (count[num][1] - 2)
+
             # sixes
             if count[num][0] == 6 and count[num][1] >= 3:
                 score += 600 * (count[num][1] - 2)
@@ -59,7 +62,6 @@ class GameLogic:
 
             # straight
             if len(count) == 6:
-                # if count[0][1] == 1 and count[1][1] == 1 and count[2][1] == 1 and count[3][1] == 1 and count[4][1] == 1 and count[5][1] ==1:
                 score = 1500
 
             # 2 triples
@@ -69,7 +71,8 @@ class GameLogic:
 
         return score
 
-    def print_dice(self, roll):
+    @staticmethod
+    def print_dice(roll):
         string = ""
         for dice in roll:
             string += f"{str(dice)}"
@@ -79,4 +82,4 @@ class GameLogic:
 
 if __name__ == '__main__':
     # print(GameLogic.calculate_score(GameLogic.roll_dice(5)))
-    print(GameLogic.calculate_score([3,3,3,5,2,4]))
+    print(GameLogic.calculate_score([3, 3, 3, 5, 2, 4]))
